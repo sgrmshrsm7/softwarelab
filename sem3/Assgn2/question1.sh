c127aebc2206b54cb8143479de1fa726653774e5
Sagar Mishra <sgrmshrsm7@gmail.com>
2020-01-26 13:44:32 +0000

    question1.sh

---
+++ sem3/Assgn2/question1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir ASSIGN2_114
+cd ASSIGN2_114
+touch PRG1_114.sh PRG2.sh
+rm PRG2.sh
+printf "Rollnos batch marks password \nBT18CSE500 R1 30 pwd1 \nBT18CSE400 R2 40 pwd2 \nBT18CSE114 R4 20 pwd3 \n" > student.txt
